426dda99a0d530da1879fd1b653873017e9e3e8d
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-08-07 09:58:36 +0530

    Update india-capitagain.py

--- india-capitagain.py
+++ india-capitagain.py
@@ -189,7 +189,7 @@ title_text = f"Capital Gain Tax Analysis for Property (Indexation vs Non-Indexat
 fig.update_layout(
     title={
         'text': title_text,
-        'y': 0.95,
+        'y': 1.2,
         'x': 0.5,
         'xanchor': 'center',
         'yanchor': 'top',
