9dc1103d038699358ef3534dbaf76672fa8eab11
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-08-07 10:31:13 +0530

    Update india-capitagain.py

--- india-capitagain.py
+++ india-capitagain.py
@@ -183,7 +183,8 @@ fig.add_hline(y=0.0, line_width=2, line_dash="dash", line_color="black")
 
 title = f"Capital Gain Tax For Property (Indexation vs NonIndexation)<span style='color:blue;'> - Purchase Year {selected_year}\
 </span> - Purchase Price <span style='color:red;'> Rs {purchase_price} Lakhs</span>\
-</span> - Selling Price <span style='color:red;'> Rs {selling_price} Lakhs</span>"
+</span> - Selling Price <span style='color:red;'> Rs {selling_price} Lakhs</span>\
+</span> - Savings with Indexation <span style='color:green;'> Rs {tax_gains_with_indexation} Lakhs</span>"
 
 
 # # Title with HTML for automatic line breaks
